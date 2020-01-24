86b3bdb33510aacdb359fd8cbf8a5d11184ca576
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2020-01-23 22:00:03 -0700

    deploying

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+require('dotenv').config()
+const server = require('./server.js');
+
+
+const port = process.env.PORT || 7000;
+
+server.listen(port, ()=> console.log('/nserver running\n', port));
